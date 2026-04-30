c48f1d313cc0bdf421869740769c988e186b19ed
Max Perkins <maxwellhperkins@gmail.com>
2026-04-30 13:23:20 -0400

    Fix ContactsView syntax errors

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import CRMApp from './CRMManager.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <CRMApp />
-  </StrictMode>,
-)
